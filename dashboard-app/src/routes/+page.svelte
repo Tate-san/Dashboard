572c2c57d85a145e703ae2e042c4f0a26cef9454
Matsvei Hauryliuk <prontx@matsveihauryliukcz@gmail.com>
2023-11-06 23:53:34 +0100

    Added gauge component

--- dashboard-app/src/routes/+page.svelte
+++ dashboard-app/src/routes/+page.svelte
@@ -2,14 +2,9 @@
     import "../app.scss";
     import { Card, Button, Toggle } from 'flowbite-svelte';
     let hCard = false;
+
+	import Gauge from './Gauge.svelte';
 </script>
 
-<div class="w-full h-screen flex flex-col justify-center items-center">
-    <h1 class="text-4xl font-bold mb-4">Hello World</h1>
-    <Card img="https://i.cbc.ca/1.5359228.1577206958!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/smudge-the-viral-cat.jpg" href="/" horizontal reverse={hCard} class="mb-4">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </Card>
-    <Toggle bind:checked={hCard} class="italic dark:text-gray-500">Reverse</Toggle>
-</div>
+<Gauge />
 
---
+++ dashboard-app/src/routes/Gauge.svelte
@@ -0,0 +1,73 @@
+<script>
+    let gaugeValue = 50; // Initial value for the gauge (between 0 and 100)
+  
+    function updateGauge(value) {
+      gaugeValue = value;
+    }
+  </script>
+  
+  <div class="gauge-container">
+    <svg class="gauge-svg" width="240" height="240">
+      <!-- Black square background -->
+      <rect x="0" y="0" width="240" height="240" fill="black"></rect>
+  
+      <!-- Gauge chart -->
+      <circle class="gauge-background" cx="120" cy="120" r="80"></circle>
+      <circle
+        class="gauge-foreground"
+        cx="120"
+        cy="120"
+        r="80"
+        :stroke-dasharray="{gaugeValue}, 100"
+      ></circle>
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" class="gauge-text">
+        {gaugeValue}%
+      </text>
+    </svg>
+  </div>
+  
+  <style>
+    .gauge-container {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+  
+    .gauge-svg {
+      width: 240px;
+      height: 240px;
+    }
+  
+    .gauge {
+      fill: none;
+    }
+  
+    .gauge-background {
+      stroke: #ccc;
+      stroke-width: 20;
+    }
+  
+    .gauge-foreground {
+      stroke: #FF5733;
+      stroke-width: 20;
+      transition: stroke-dasharray 0.5s ease-in-out;
+    }
+  
+    .gauge-text {
+      font-size: 24px;
+      font-weight: 600;
+      font-family:monospace;
+      fill: #fff; 
+    }
+  
+    .buttons {
+      text-align: center;
+    }
+  
+    button {
+      margin: 10px;
+      padding: 10px 20px;
+      font-size: 16px;
+    }
+  </style>
+
+  
