4b5ee81b31f8b6155ac6db070566673ce6becb4b
ENIACx64 <jindrich.vodak@email.cz>
2023-11-27 01:17:19 +0100

    feat: added system detail page

---
+++ dashboard-app/src/routes/system/+page.svelte
@@ -0,0 +1,18 @@
+<script> 
+    import { onMount } from 'svelte';
+    import MainLayout from "../../layouts/MainLayout.svelte";
+    import { getSystemListByUsers } from '../../hooks/systems';
+
+    let systemId;
+
+    onMount(async () => {
+        const queryParams = new URLSearchParams(window.location.search);
+        systemId = queryParams.get('id');
+  });
+</script>
+  
+<MainLayout>
+    <div class="w-full h-full px-10">
+        {systemId}
+    </div>
+</MainLayout>
